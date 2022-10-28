a10eb3442465f2e69571b1f74c95cca028c04e09
yeisontrillos <yeisontrillospaez@gmail.com>
2022-10-27 21:09:59 -0500

    quinto commit tercera parte se modifico el registro de un nuevo usuario multiusuarios en el formulario

--- personal/nuevousuariop.php
+++ personal/nuevousuariop.php
@@ -64,11 +64,22 @@ body {
 </style>
 
 <body class="body__edit">
+<?php 
+      if($_POST) {
+        $conexion = mysqli_connect("bq0blsp5cjqgnsb6of7v-mysql.services.clever-cloud.com", "uhxjobwzbkzkkimo", "b392blez1n8d9gzyXV4p", "bq0blsp5cjqgnsb6of7v") or
+        die("Problemas con la conexión");
     
+      mysqli_query($conexion, "insert into mainlogin (id ,username ,email ,password ,role) values 
+                           ('$_REQUEST[id]','$_REQUEST[username]','$_REQUEST[email]','$_REQUEST[password]','$_REQUEST[role]')")
+        or die("Problemas en el select" . mysqli_error($conexion));
+        mysqli_close($conexion);
+        header("location: personal_portada.php");   
+      }
+      ?> 
 
 
 <div class="login-form">  
-<form action="conexionreg.php" method="post" class="form-horizontal" style="background-color:#dde1ef; border-radius:12px; width:100%; ">
+<form action="nuevousuariop.php" method="post" class="form-horizontal" style="background-color:#dde1ef; border-radius:12px; width:100%; ">
 <center><h2 class="titulo_regis">Registrarse</h2></center>
 <div class="form-group">
 <label  class="col-sm-9 text-left">Usuario</label>
