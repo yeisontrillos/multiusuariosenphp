f638bcfc16f496152f5b0ca02c338b0639cad983
yeisontrillos <yeisontrillospaez@gmail.com>
2022-10-27 20:59:20 -0500

    quinto commit segunda parte se modifico el registro de un nuevo usuario multiusuarios en la conexion

--- personal/conexionreg.php
+++ personal/conexionreg.php
@@ -3,7 +3,7 @@
         $conexion = mysqli_connect("bq0blsp5cjqgnsb6of7v-mysql.services.clever-cloud.com", "uhxjobwzbkzkkimo", "b392blez1n8d9gzyXV4p", "bq0blsp5cjqgnsb6of7v") or
         die("Problemas con la conexión");
     
-      mysqli_query($conexion, "insert into mainlogin (id ,username ,email ,password ,role) values 
+      mysqli_query($conexion, "INSERT INTO mainlogin (`id` ,`username` ,`email` ,`password` ,`role`) VALUES
                            ('$_REQUEST[id]','$_REQUEST[username]','$_REQUEST[email]','$_REQUEST[password]','$_REQUEST[role]')")
         or die("Problemas en el select" . mysqli_error($conexion));
         echo "<script>location.href='personal_portada.php'</script>";
